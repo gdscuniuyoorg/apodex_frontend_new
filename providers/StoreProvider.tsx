df56d212647fb0db22085d0b084f1af71f130df3
Victor-Okpukpan <darksaand@gmail.com>
2024-03-12 11:17:56 +0100

    feat: add Redux Toolkit for state management

---
+++ providers/StoreProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { store } from "../store/store";
+import { Provider } from "react-redux";
+
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Provider store={store}>{children}</Provider>;
+}
