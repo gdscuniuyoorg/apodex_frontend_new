47eb6ad8e7aa00e78d11673669b1d9e057af8d3c
AirZED <regnalukpabio@gmail.com>
2024-03-13 02:15:12 +0100

    first commit

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,26 +1,32 @@
-import React, { ChangeEvent, FunctionComponent } from 'react';
+import React, { ChangeEvent, FunctionComponent } from "react";
 
 interface InputBoxProps {
- label?: string;
- isTextArea?: boolean;
- type: string;
- id: string;
- value: string | number;
- handleChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
- accept?: string;
+  label?: string;
+  isTextArea?: boolean;
+  type: string;
+  id: string;
+  value: string | number;
+  handleChange: (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  accept?: string;
 }
 
 const InputBox: FunctionComponent<InputBoxProps> = ({
- label,
- isTextArea,
- type,
- id,
- value,
- handleChange,
- accept,
+  label,
+  isTextArea,
+  type,
+  id,
+  value,
+  handleChange,
+  accept,
 }) => {
- return (
-    <div className={`${type !== "file" && "border shadow-inner"} font-work-sans w-full border-black px-4 py-3 rounded-lg shadow-[#4a4a6826] mb-5 flex flex-col`}>
+  return (
+    <div
+      className={`${
+        type !== "file" && "border shadow-inner"
+      } font-work-sans w-full border-black px-4 py-3 rounded-lg shadow-[#4a4a6826] mb-5 flex flex-col`}
+    >
       {label && (
         <label htmlFor={id} className="font-medium text-base">
           {label}
@@ -43,11 +49,13 @@ const InputBox: FunctionComponent<InputBoxProps> = ({
           value={value.toString()}
           onChange={handleChange}
           accept={accept}
-          className={`w-full outline-none bg-transparent ${type === "file" && "hidden"}`}
+          className={`w-full outline-none bg-transparent ${
+            type === "file" && "hidden"
+          }`}
         />
       )}
     </div>
- );
+  );
 };
 
 export default InputBox;
