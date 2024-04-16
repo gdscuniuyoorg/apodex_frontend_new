044861108cfedd92c92acd24e83ef0fe013c9933
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-04-17 00:15:04 +0100

    couldnt even complete a line,dont push to main

---
+++ app/dashboard/challenges/Button.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+
+interface ButtonProps {
+    children: ReactNode;
+    className: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, className }) => {
+  return (
+    <button
+      className={`${className} flex flex-row gap-3 items-center bg-[#0070CC] rounded-[4 px] text-white p-3`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- app/dashboard/challenges/page.tsx
+++ app/dashboard/challenges/page.tsx
@@ -1,5 +1,17 @@
-const Challenges = () => {
-  return <div className="container">This is the homepage</div>;
-};
+import React from 'react'
+import Button from './Button'
+import ChallengeIcon from '@/components/Icons/ChallengeIcon';
 
-export default Challenges;
+export default function Challenge() {
+  return (
+    <div>
+      <div className="flex flex-row items-center justify-between p-5">
+        <h3 className="font-semibold text-[33px] leadding-[43.56px] text-[#535458]">
+          Challenges
+        </h3>
+        <Button className=''>   Create a challenge</Button>
+      </div>
+    </div>
+  );
+}
+
