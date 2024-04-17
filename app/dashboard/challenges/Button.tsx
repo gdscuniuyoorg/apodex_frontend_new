0d430a1f0474230bea88c28976f3b0c393202d80
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-04-17 14:25:27 +0100

    done with the ui

--- app/dashboard/challenges/Button.tsx
+++ app/dashboard/challenges/Button.tsx
@@ -8,8 +8,9 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, className }) => {
   return (
     <button
-      className={`${className} flex flex-row gap-3 items-center bg-[#0070CC] rounded-[4 px] text-white p-3`}
-    >
+      className={`${className} flex flex-row gap-3 items-center bg-[#0070CC] rounded-[4px] text-white p-3`}
+      >
+          
       {children}
     </button>
   );
--- app/dashboard/challenges/page.tsx
+++ app/dashboard/challenges/page.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import Button from './Button'
-import ChallengeIcon from '@/components/Icons/ChallengeIcon';
+import Challengeimg from './assets/Challengeimg';
+import Vector from './assets/vector';
 
 export default function Challenge() {
   return (
     <div>
-      <div className="flex flex-row items-center justify-between p-5">
+      <div className="flex flex-row items-center justify-between p-10">
         <h3 className="font-semibold text-[33px] leadding-[43.56px] text-[#535458]">
           Challenges
         </h3>
-        <Button className=''>   Create a challenge</Button>
+        <Button className="">
+          <Vector></Vector>
+          <p>Create a challenge</p>
+        </Button>
+      </div>
+      <div className="flex flex-column items-center justify-center">
+        <Challengeimg></Challengeimg>
+      </div>
+      <div className="text-center m-auto w-[35%]">
+        <h4 className="text-[#535458] font-semibold p-2">
+          Let’s Kick Things Off with a Challenge
+        </h4>
+        <p className="text-[#88898C] text-[13px]">
+          You have not created any Challenge yet. You can click the button below
+          or above to start. You will be able to access information regarding to
+          Challenges on this page
+        </p>
+      </div>
+      <div className="flex items-center justify-center p-5">
+        <Button className="">
+          <Vector></Vector>
+          <p>Create a challenge</p>
+        </Button>
       </div>
     </div>
   );
