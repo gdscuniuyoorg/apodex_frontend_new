5aed694bfff01400f28c1a0e32da4ccdf9a6296b
kufre-abasi <kufreabasibassey3@gmail.com>
2024-04-17 22:53:40 +0100

    docs: landing page buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import Link from 'next/link';
+
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  gap-6 justify-center p-24">
       This is for the landing page maybe?
+      <div className="w-full flex items-center justify-center gap-4">
+        <Link
+          href="/login"
+          className="font-light flex gap-3 btn-brand items-center"
+        >
+          <span className="text-[1.7rem]  flex items-center justify-center">
+            Login{' '}
+          </span>
+        </Link>
+        <Link
+          href="/sign-up"
+          className="font-light flex gap-3 btn-brand items-center"
+        >
+          <span className="text-[1.7rem]  flex items-center justify-center">
+            Sign up{' '}
+          </span>
+        </Link>
+      </div>
     </main>
   );
 };
