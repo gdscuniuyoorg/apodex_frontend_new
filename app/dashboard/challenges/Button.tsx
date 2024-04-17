a5aee860d60a67cc334e65e9a20bf4e108b061f2
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-04-17 15:50:39 +0100

    updated css

--- app/dashboard/challenges/Button.tsx
+++ app/dashboard/challenges/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, className }) => {
   return (
     <button
-      className={`${className} flex flex-row gap-3 items-center bg-[#0070CC] rounded-[4px] text-white p-3`}
+      className={`${className} get-started flex flex-row gap-3 items-center bg-[#0070CC] rounded-[4px] text-white p-3`}
       >
           
       {children}
--- app/dashboard/challenges/page.tsx
+++ app/dashboard/challenges/page.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Button from './Button'
 import Challengeimg from './assets/Challengeimg';
 import Vector from './assets/vector';
+import '../challenges/Challenges.css';
 
 export default function Challenge() {
   return (
     <div>
-      <div className="flex flex-row items-center justify-between p-10">
-        <h3 className="font-semibold text-[33px] leadding-[43.56px] text-[#535458]">
+      <div className="wrapper flex flex-row items-center justify-between p-10">
+        <h3 className="first font-semibold text-[33px] leadding-[43.56px] text-[#535458]">
           Challenges
         </h3>
         <Button className="">
@@ -16,13 +17,13 @@ export default function Challenge() {
         </Button>
       </div>
       <div className="flex flex-column items-center justify-center">
-        <Challengeimg></Challengeimg>
+        <Challengeimg className='ill'></Challengeimg>
       </div>
-      <div className="text-center m-auto w-[35%]">
+      <div className="empty text-center m-auto w-[35%]">
         <h4 className="text-[#535458] font-semibold p-2">
           Let’s Kick Things Off with a Challenge
         </h4>
-        <p className="text-[#88898C] text-[13px]">
+        <p className="main text-[#88898C] text-[13px]">
           You have not created any Challenge yet. You can click the button below
           or above to start. You will be able to access information regarding to
           Challenges on this page
