eb8023e2da489974b7c6c50f7b1dc1dc733eeb90
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-04-29 23:07:29 +0100

    layout rework

--- components/Layout/SideBar.tsx
+++ components/Layout/SideBar.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
 
 import NavLink from "../atoms/NavLink";
 import { GoHomeFill } from "react-icons/go";
@@ -19,28 +18,49 @@ import JobListingIcon from "@/components/Icons/JobListingIcon";
 import ProfileIcon from "@/components/Icons/ProfileIcon";
 import SettingsIcon from "@/components/Icons/SettingsIcon";
 import SupportAgentIcon from "@/components/Icons/SupportAgentIcon";
-const SideBar = ({ className }: { className: string }) => {
+import { useRouter } from "next/navigation";
+
+
+type SideBarProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+
+const SideBar = ({ open, setOpen}: SideBarProps) => {
+  const router = useRouter();
+
   return (
     <aside
-      className={`flex flex-col bg-lightBlue-100 justify-between ${className}`}
+      className={`${open ? "left-0" : "left-[-100%] lg:left-0"} min-w-[240px] transition-all w-[240px] max-w-[240px] top-0 h-screen fixed lg:relative flex flex-col bg-white justify-between p-[20px] z-20`}
     >
-      <div className="flex flex-col ">
-        <div className=" border-b-[1px] border-[#C2C2C4]/[50%] flex gap-[1.5rem] flex-col pb-[1.5rem] mb-[1.5rem]">
-          <NavLink name="Home" icon={<HomeIcon />} />
-          <NavLink name="Talents" icon={<TalentIcon />} />{' '}
-          <NavLink name="Forum" icon={<ForumIcon />} />{' '}
-          <NavLink name="Challenges" icon={<ChallengeIcon />} />{' '}
-          <NavLink name="Updates" icon={<UpdatesIcon />} />
-          <NavLink name="Job Listings" icon={<JobListingIcon />} />
-        </div>
-        <div className=" flex gap-[1.5rem] flex-col">
-          <NavLink name="Profiles" icon={<ProfileIcon />} />
-          <NavLink name="Notifications" icon={<NotificationsIcon />} />
-          <NavLink name="Settings" icon={<SettingsIcon />} />
-        </div>
+      <div className="flex flex-col grow items-center ">
+      <img
+        onClick={() => router.push(`/dashboard`)}
+        role="button"
+        src="/apodexLogo.png"
+        alt="apodex-logo"
+        className="lg:h-[3rem] h-[3rem] w-[150px]"
+      />
+      <div className="flex flex-col w-full space-y-[15px] lg:space-y-[25px] mt-[15%]">
+      {
+        links.slice(0,6).map((link, index) => (
+          <NavLink name={link.name} icon={link.icon} key={index}/>
+        ))
+      }
       </div>
-      <div className="flex flex-col justify-between mb-[4.5rem] gap-3 items-center">
-        <div className="text-[#a7a9ae] flex w-full items-center justify-start gap-3">
+      <div className="flex flex-col w-full space-y-[15px] lg:space-y-[25px] border-t border-[#C2C2C44D] pt-[10px] mt-[10%]">
+      {
+        links.slice(6,links.length).map((link, index) => (
+          <NavLink name={link.name} icon={link.icon} key={index}/>
+        ))
+      }
+      </div>
+       
+      </div>
+      
+      <div className="flex flex-col justify-between w-full gap-3 items-center">
+        <div className="text-[#a7a9ae] flex w-full items-center justify-start gap-3 ">
           <img
             className="rounded-full w-[40px] h-[40px] object-cover"
             src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg"
@@ -51,7 +71,7 @@ const SideBar = ({ className }: { className: string }) => {
             <p className="text-neutral-300 text-md">UX Designer</p>
           </div>
         </div>
-        <button className="border-[1px] rounded-md p-2 border-[#C2C2C4]/[50%] flex items-center  gap-2 ">
+        <button className="border-[1px] rounded-md p-2 border-[#C2C2C4]/[50%] flex items-center  gap-2 w-full justify-center">
           <SupportAgentIcon />
           <p className="text-neutral-300">Contact Support</p>
         </button>
@@ -61,3 +81,42 @@ const SideBar = ({ className }: { className: string }) => {
 };
 
 export default SideBar;
+
+const links = [
+  {
+    name: "Home",
+    icon: <HomeIcon />,
+  },
+  {
+    name: "Talents",
+    icon: <TalentIcon />,
+  },
+  {
+    name: "Forum",
+    icon: <ForumIcon />,
+  },
+  {
+    name: "Challenges",
+    icon: <ChallengeIcon />,
+  },
+  {
+    name: "Updates",
+    icon: <UpdatesIcon />,
+  },
+  {
+    name: "Job Listings",
+    icon: <JobListingIcon />,
+  },
+  {
+    name: "Profiles",
+    icon: <ProfileIcon />,
+  },
+  {
+    name: "Notifications",
+    icon: <NotificationsIcon />,
+  },
+  {
+    name: "Settings",
+    icon: <SettingsIcon />,
+  },
+];
